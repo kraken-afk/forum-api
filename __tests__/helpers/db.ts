06416247f5bed7c620a353bfd089a6fc3b15fcb4
Romeo Noveanre <noveanrer@gmail.com>
2023-11-30 00:01:48 +0700

    test ci (3)

--- __tests__/helpers/db.ts
+++ __tests__/helpers/db.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { createPool } from '~/infrastructure/database/create-pool';
 
 import { config } from 'dotenv';
+import postgres from 'postgres';
 
 config();
 
@@ -11,16 +12,19 @@ const {
   PGDATABASE_TEST,
   PGPASSWORD_TEST,
   PGPORT_TEST,
+  PLATFORM,
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 } = process.env as any;
 
 export const db = drizzle(
-  createPool({
-    host: PGHOST_TEST,
-    port: PGPORT_TEST,
-    database: PGDATABASE_TEST,
-    password: PGPASSWORD_TEST,
-    username: PGUSER_TEST,
-    max: 1,
-  }),
+  PLATFORM === 'ACTION'
+    ? postgres(process.env.DATBASE_URL!)
+    : createPool({
+        host: PGHOST_TEST,
+        port: PGPORT_TEST,
+        database: PGDATABASE_TEST,
+        password: PGPASSWORD_TEST,
+        username: PGUSER_TEST,
+        max: 1,
+      }),
 );
