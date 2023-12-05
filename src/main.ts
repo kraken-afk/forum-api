c15de1bd29d3625094b79aa92138c22d85d2e566
Romeo Noveanre <noveanrer@gmail.com>
2023-12-05 21:06:54 +0700

    test using docker (11)

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import { server } from '~/interfaces/http/core/server';
 const port = parseInt(process.env.PORT ?? '3000', 10);
 const host = process.env?.HOST as string;
 
+console.log(port, host);
+
 void config();
 server('src/api', host, port);
